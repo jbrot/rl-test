8d41858a87bd9538320e855fc2dcf97b36c342f9
Joshua Brot <jbrot@umich.edu>
2019-12-01 04:35:58 -0600

    Implemented Adam.
    There is a lot to be cleaned up here.... Also, it didn't work :( I guess
    on to ensuring the gradients are calculated correctly.

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE DataKinds, ScopedTypeVariables, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE ConstraintKinds, DataKinds, ScopedTypeVariables, TypeFamilies, TypeOperators #-}
 module Main where
 
 import Control.Monad
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Control
 import Control.Monad.Trans.State
+import Data.MonoTraversable (MonoFunctor(..))
+import Data.Singletons.TypeLits
 import Data.VectorSpace
+import GHC.Types (Constraint)
 import Grenade
 import Graphics.Gloss.Interface.IO.Simulate
 import Graphics.Gloss.Data.ViewPort
@@ -20,9 +23,48 @@ data IState g = IState { gym :: g
                        , obs :: Observation g
                        , nn :: NNet
                        , rollout :: [(Float,Gradients NL)]
+                       , adam :: Adam
                        , episode :: Int
                        , avg :: Float
                        }
+
+data Adam = Adam { alpha :: Double
+                 , beta1 :: Double
+                 , beta2 :: Double
+                 , epsilon :: Double
+                 , mom :: Gradients NL
+                 , vel :: Gradients NL
+                 , time :: Int
+                 }
+
+defAdam :: Adam
+defAdam = Adam 0.001 0.9 0.999 1e-8 zeroV zeroV 0
+
+class UpdateLayer x => UpdateLayerRaw x where
+    runUpdateRaw :: Gradient x -> x -> x
+
+instance (KnownNat i, KnownNat o) => UpdateLayerRaw (FullyConnected i o) where
+    runUpdateRaw d (FullyConnected a b) = FullyConnected (d ^+^ a) b
+instance UpdateLayerRaw (Relu) where
+    runUpdateRaw _ _ = Relu
+instance UpdateLayerRaw (Softmax) where
+    runUpdateRaw _ _ = Softmax
+
+type family AllULR (as :: [*]) :: Constraint where
+    AllULR '[] = ()
+    AllULR (a ': as) = (UpdateLayerRaw a, AllULR as)
+
+applyRaw :: AllULR layers => Gradients layers -> Network layers shapes -> Network layers shapes
+applyRaw GNil NNil = NNil
+applyRaw (g :/> gs) (n :~> ns) = (runUpdateRaw g n :~> ns)
+
+runAdam :: Adam -> Gradients NL -> NNet -> (Adam, NNet)
+runAdam a g n = (a{mom = m, vel = v, time = t}, applyRaw del n)
+  where t = 1 + (time a)
+        m = (beta1 a) *^ (mom a) ^+^ (1 - beta1 a) *^ g
+        v = (beta2 a) *^ (vel a) ^+^ (1 - beta2 a) *^ (omap (^2) g)
+        at = (alpha a) * sqrt (1 - (beta2 a)^t) / (1 - (beta1 a)^t)
+        del = oliftA2 (\x y -> (-at) * x / (sqrt y + epsilon a)) m v
 
 istate :: (Monad m) => (g -> (a,g)) -> StateT (IState g) m a
 istate f = do
@@ -31,7 +73,7 @@ istate f = do
     pure out
 
 defSt :: Gym g => IO (IState g)
-defSt = fmap reset start >>= \(o, g) -> IState g o <$> randNN <*> pure [] <*> pure 0 <*> pure 0
+defSt = fmap reset start >>= \(o, g) -> IState g o <$> randNN <*> pure [] <*> pure defAdam <*> pure 0 <*> pure 0
 
 rnd :: Gym g => IState g -> IO Picture
 rnd = pure . translate (-300) (-200) . render 600 400 . gym
@@ -39,7 +81,7 @@ rnd = pure . translate (-300) (-200) . render 600 400 . gym
 rtf = realToFrac
 
 updateNet :: IState Cartpole -> IState Cartpole
-updateNet st = st{rollout = [], nn = applyUpdate (LearningParameters 0.01 0.9 0.005) (nn st) upd}
+updateNet st = st{rollout = [], adam = ad', nn = nn'}
   where gamma = 0.9
         (_,gtrl) = foldl (\(v,ls) (r,g) -> let nv = gamma * v + r in (nv, (nv,g):ls)) (0, []) (rollout st)
         average :: Fractional n => [n] -> n
@@ -48,7 +90,8 @@ updateNet st = st{rollout = [], nn = applyUpdate (LearningParameters 0.01 0.9 0.
         avg = average (fmap fst gtrl) 
         stdev :: Float
         stdev = sqrt . average . fmap (\(x,_) -> (x - avg)^2) $ gtrl
-        upd = foldr (\(x,g) ag -> ag ^+^ ((rtf $ (avg - x) / (stdev + 1e-9)) *^ g)) zeroV gtrl
+        upd = foldr (\(x,g) ag -> ag ^+^ ((rtf $ (x - avg) / (stdev + 1e-9)) *^ g)) zeroV gtrl
+        (ad', nn') = runAdam (adam st) upd (nn st)
 
 resetEp :: MonadIO m => StateT (IState Cartpole) m (Observation Cartpole)
 resetEp = do
--- app/NN.hs
+++ app/NN.hs
@@ -3,6 +3,7 @@ module NN where
 
 import Control.Monad.IO.Class
 import Data.Maybe (fromJust)
+import Data.MonoTraversable (MonoFunctor(..), Element)
 import Data.Proxy
 import Data.Singletons
 import Data.Singletons.Prelude.Bool
@@ -13,6 +14,8 @@ import qualified Data.Vector.Storable as V
 import Data.VectorSpace
 import Grenade
 import Numeric.LinearAlgebra.Static
+import qualified Numeric.LinearAlgebra as L
+import qualified Numeric.LinearAlgebra.Devel as L
 import System.Random
 
 type NL = '[ FullyConnected 4 128, Relu, FullyConnected 128 2, Softmax ]
@@ -39,29 +42,57 @@ apply nn v = fmap (\t -> (t, fst . runGradient nn tape . S1D . fromJust . create
 
 -- Orphan instances to make Gradients a vector space.
 
+-- TBD: How does this work with more than two params?
+class MonoFunctor f => MonoApplicative f where
+    opure :: Element f -> f
+    oliftA2 :: (Element f -> Element f -> Element f) -> f -> f -> f
+
+zipMats :: (KnownNat i, KnownNat o) => (Double -> Double -> Double) -> L i o -> L i o -> L i o
+zipMats f a b = fromJust . create . L.reshape (snd $ size a) $ L.zipVectorWith f (L.flatten $ extract a) (L.flatten $ extract b)
+
+
+type instance Element (FullyConnected' i o) = Double
+instance (KnownNat i, KnownNat o) => MonoFunctor (FullyConnected' i o) where
+    omap f (FullyConnected' b a)  = FullyConnected' (dvmap f b) (dmmap f a)
+instance (KnownNat i, KnownNat o) => MonoApplicative (FullyConnected' i o) where
+    opure c = FullyConnected' (konst c) (konst c)
+    oliftA2 f (FullyConnected' a b) (FullyConnected' a2 b2) =  FullyConnected' (zipWithVector f a a2) (zipMats f b b2)
 instance (KnownNat i, KnownNat o) => AdditiveGroup (FullyConnected' i o) where
-    zeroV = FullyConnected' (konst 0) (konst 0)
-    (FullyConnected' b a) ^+^ (FullyConnected' b2 a2) = FullyConnected' (b + b2) (a + a2)
-    negateV (FullyConnected' b a) = FullyConnected' (-b) (-a)
+    zeroV = opure 0
+    (^+^) = oliftA2 (+)
+    negateV = omap negate
 instance (KnownNat i, KnownNat o) => VectorSpace (FullyConnected' i o) where
     type Scalar (FullyConnected' i o) = Double
-    c *^ (FullyConnected' b a) = FullyConnected' (dvmap (*c) b) (dmmap (*c) a)
+    c *^ v = omap (c *) v
 
-instance AdditiveGroup (Gradients '[]) where
-    zeroV = GNil
-    GNil ^+^ GNil = GNil
-    negateV GNil = GNil
-instance VectorSpace (Gradients '[]) where
-    type Scalar (Gradients '[]) = Double
-    c *^ GNil = GNil
+type instance Element (Gradients '[]) = Double
+instance MonoFunctor (Gradients '[]) where
+    omap f GNil = GNil
+instance MonoApplicative (Gradients '[]) where
+    opure _ = GNil
+    oliftA2 _ GNil GNil = GNil
 
-instance (AdditiveGroup (Gradients as), AdditiveGroup (Gradient a), UpdateLayer a) => AdditiveGroup (Gradients (a ': as)) where
-    zeroV = zeroV :/> zeroV
-    (a :/> b) ^+^ (c :/> d) = (a ^+^ c) :/> (b ^+^ d)
-    negateV (a :/> b) = (negateV a) :/> (negateV b)
-instance (VectorSpace (Gradients as), VectorSpace (Gradient a), Scalar(Gradient a) ~ Scalar(Gradients as), UpdateLayer a) => VectorSpace (Gradients (a ': as)) where
-    type Scalar (Gradients (a ': as)) = Scalar (Gradients as)
-    c *^ (a :/> b) = (c *^ a) :/> (c *^ b)
+type instance Element (Gradients (a ': as)) = Element (Gradient a)
+instance (MonoFunctor (Gradients as), MonoFunctor (Gradient a), UpdateLayer a, Element (Gradients as) ~ Element (Gradient a)) => MonoFunctor (Gradients (a ': as)) where
+    omap f (a :/> b) = (omap f a) :/> (omap f b)
+instance (MonoApplicative (Gradients as), MonoApplicative (Gradient a), UpdateLayer a, Element (Gradients as) ~ Element (Gradient a)) => MonoApplicative (Gradients (a ': as)) where
+    opure c = (opure c) :/> (opure c)
+    oliftA2 f (a :/> b) (a2 :/> b2) = (oliftA2 f a a2) :/> (oliftA2 f b b2)
+
+instance (MonoApplicative (Gradients as), Num (Element (Gradients as))) => AdditiveGroup (Gradients as) where
+    zeroV = opure 0
+    (^+^) = oliftA2 (+)
+    negateV = omap negate
+instance (AdditiveGroup (Gradients as), MonoFunctor (Gradients as), Num (Element (Gradients as))) => VectorSpace (Gradients as) where
+    type Scalar (Gradients as) = Element (Gradients as)
+    c *^ v = omap (c *) v
+
+type instance Element () = Double
+instance MonoFunctor () where
+    omap f () = ()
+instance MonoApplicative () where
+    opure _ = ()
+    oliftA2 _ () () = ()
 instance VectorSpace () where
     type Scalar () = Double
-    c *^ () = ()
+    c *^ v = omap (c *) v
